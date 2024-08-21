51d7a495b1e50aa955e183b9152c5ca0122ff44b
Дмитрий <dmitklgjob@gmail.com>
2024-08-21 10:47:40 +0300

    fix-errors

--- php/v2/ReturnOperation.php
+++ php/v2/ReturnOperation.php
@@ -2,129 +2,163 @@
 
 namespace NW\WebService\References\Operations\Notification;
 
+use Exception;
+
 class TsReturnOperation extends ReferencesOperation
 {
-    public const TYPE_NEW    = 1;
+    public const TYPE_NEW = 1;
     public const TYPE_CHANGE = 2;
 
     /**
-     * @throws \Exception
+     * @throws Exception
      */
-    public function doOperation(): void
+    public function doOperation(): array
     {
-        $data = (array)$this->getRequest('data');
-        $resellerId = $data['resellerId'];
-        $notificationType = (int)$data['notificationType'];
+        $data = $this->getRequest('data');
+        $mapper = new DataMapper($data);
+
         $result = [
             'notificationEmployeeByEmail' => false,
-            'notificationClientByEmail'   => false,
-            'notificationClientBySms'     => [
-                'isSent'  => false,
+            'notificationClientByEmail' => false,
+            'notificationClientBySms' => [
+                'isSent' => false,
                 'message' => '',
             ],
         ];
 
-        if (empty((int)$resellerId)) {
+        if ($mapper->getResellerId() === null) {
             $result['notificationClientBySms']['message'] = 'Empty resellerId';
             return $result;
         }
 
-        if (empty((int)$notificationType)) {
-            throw new \Exception('Empty notificationType', 400);
+
+        if (!$mapper->getResellerId()) {
+            throw new Exception('Invalid resellerId', 400);
         }
 
-        $reseller = Seller::getById((int)$resellerId);
-        if ($reseller === null) {
-            throw new \Exception('Seller not found!', 400);
+        if ($mapper->getNotificationType() === null) {
+            throw new Exception('Empty notificationType', 400);
         }
 
-        $client = Contractor::getById((int)$data['clientId']);
-        if ($client === null || $client->type !== Contractor::TYPE_CUSTOMER || $client->Seller->id !== $resellerId) {
-            throw new \Exception('сlient not found!', 400);
+
+        if (!in_array($mapper->getNotificationType(), [self::TYPE_NEW, self::TYPE_CHANGE])) {
+            throw new Exception('Invalid notificationType', 400);
         }
 
-        $cFullName = $client->getFullName();
-        if (empty($client->getFullName())) {
-            $cFullName = $client->name;
+        $reseller = Seller::getById($mapper->getResellerId());
+
+        if (!$reseller) {
+            throw new Exception('Seller not found!', 400);
         }
 
-        $cr = Employee::getById((int)$data['creatorId']);
+        if ($mapper->getClientId() === null) {
+            throw new Exception('Invalid clientId', 400);
+        }
+
+        $client = Contractor::getById($mapper->getClientId());
+        if (!$client || $client->type !== Contractor::TYPE_CUSTOMER || $client->Seller->id !== $mapper->getResellerId()) {
+            throw new Exception('Client not found!', 400);
+        }
+
+        if ($mapper->getCreatorId()) {
+            throw new Exception('Invalid creatorId', 400);
+        }
+
+        $cr = Employee::getById($mapper->getCreatorId());
         if ($cr === null) {
-            throw new \Exception('Creator not found!', 400);
+            throw new Exception('Creator not found!', 400);
         }
 
-        $et = Employee::getById((int)$data['expertId']);
+        if (!$mapper->getExpertId()) {
+            throw new Exception('Invalid expertId', 400);
+        }
+
+        $et = Employee::getById($mapper->getExpertId());
         if ($et === null) {
-            throw new \Exception('Expert not found!', 400);
+            throw new Exception('Expert not found!', 400);
         }
 
         $differences = '';
-        if ($notificationType === self::TYPE_NEW) {
-            $differences = __('NewPositionAdded', null, $resellerId);
-        } elseif ($notificationType === self::TYPE_CHANGE && !empty($data['differences'])) {
-            $differences = __('PositionStatusHasChanged', [
-                    'FROM' => Status::getName((int)$data['differences']['from']),
-                    'TO'   => Status::getName((int)$data['differences']['to']),
-                ], $resellerId);
+
+        if ($mapper->getNotificationType() === self::TYPE_NEW) {
+            $differences = ['NewPositionAdded', null, $mapper->getResellerId()];
+        } elseif ($mapper->getNotificationType() === self::TYPE_CHANGE && $mapper->isDifferencesFilled()) {
+            $differences = ['PositionStatusHasChanged', [
+                'FROM' => Status::getName($mapper->getDifferencesFrom()),
+                'TO' => Status::getName($mapper->getDifferencesTo()),
+            ], $mapper->getResellerId()];
         }
 
         $templateData = [
-            'COMPLAINT_ID'       => (int)$data['complaintId'],
-            'COMPLAINT_NUMBER'   => (string)$data['complaintNumber'],
-            'CREATOR_ID'         => (int)$data['creatorId'],
-            'CREATOR_NAME'       => $cr->getFullName(),
-            'EXPERT_ID'          => (int)$data['expertId'],
-            'EXPERT_NAME'        => $et->getFullName(),
-            'CLIENT_ID'          => (int)$data['clientId'],
-            'CLIENT_NAME'        => $cFullName,
-            'CONSUMPTION_ID'     => (int)$data['consumptionId'],
-            'CONSUMPTION_NUMBER' => (string)$data['consumptionNumber'],
-            'AGREEMENT_NUMBER'   => (string)$data['agreementNumber'],
-            'DATE'               => (string)$data['date'],
-            'DIFFERENCES'        => $differences,
+            'COMPLAINT_ID' => $mapper->getComplaintId(),
+            'COMPLAINT_NUMBER' => $mapper->getComplaintNumber(),
+            'CREATOR_ID' => $mapper->getCreatorId(),
+            'CREATOR_NAME' => $cr->getFullName(),
+            'EXPERT_ID' => $mapper->getExpertId(),
+            'EXPERT_NAME' => $et->getFullName(),
+            'CLIENT_ID' => $mapper->getClientId(),
+            'CLIENT_NAME' => $client->getFullName(),
+            'CONSUMPTION_ID' => $mapper->getConsumptionId(),
+            'CONSUMPTION_NUMBER' => $mapper->getConsumptionNumber(),
+            'AGREEMENT_NUMBER' => $mapper->getAgreementNumber(),
+            'DATE' => $mapper->getDate(),
+            'DIFFERENCES' => $differences,
         ];
 
         // Если хоть одна переменная для шаблона не задана, то не отправляем уведомления
         foreach ($templateData as $key => $tempData) {
             if (empty($tempData)) {
-                throw new \Exception("Template Data ({$key}) is empty!", 500);
+                throw new Exception("Template Data ({$key}) is empty!", 500);
             }
         }
 
-        $emailFrom = getResellerEmailFrom($resellerId);
+        $emailFrom = getResellerEmailFrom($mapper->getResellerId());
         // Получаем email сотрудников из настроек
-        $emails = getEmailsByPermit($resellerId, 'tsGoodsReturn');
-        if (!empty($emailFrom) && count($emails) > 0) {
+        $emails = getEmailsByPermit($mapper->getResellerId(), 'tsGoodsReturn');
+        if (!$emailFrom && count($emails) > 0) {
             foreach ($emails as $email) {
                 MessagesClient::sendMessage([
-                    0 => [ // MessageTypes::EMAIL
-                           'emailFrom' => $emailFrom,
-                           'emailTo'   => $email,
-                           'subject'   => __('complaintEmployeeEmailSubject', $templateData, $resellerId),
-                           'message'   => __('complaintEmployeeEmailBody', $templateData, $resellerId),
+                    [ // MessageTypes::EMAIL
+                        'emailFrom' => $emailFrom,
+                        'emailTo' => $email,
+                        'subject' => ['complaintEmployeeEmailSubject', $templateData, $mapper->getResellerId()],
+                        'message' => ['complaintEmployeeEmailBody', $templateData, $mapper->getResellerId()],
                     ],
-                ], $resellerId, NotificationEvents::CHANGE_RETURN_STATUS);
-                $result['notificationEmployeeByEmail'] = true;
+                ], $mapper->getResellerId(), NotificationEvents::CHANGE_RETURN_STATUS);
 
+                $result['notificationEmployeeByEmail'] = true;
             }
         }
 
         // Шлём клиентское уведомление, только если произошла смена статуса
-        if ($notificationType === self::TYPE_CHANGE && !empty($data['differences']['to'])) {
+        if ($mapper->getNotificationType() === self::TYPE_CHANGE && $mapper->getDifferencesTo()) {
             if (!empty($emailFrom) && !empty($client->email)) {
-                MessagesClient::sendMessage([
-                    0 => [ // MessageTypes::EMAIL
-                           'emailFrom' => $emailFrom,
-                           'emailTo'   => $client->email,
-                           'subject'   => __('complaintClientEmailSubject', $templateData, $resellerId),
-                           'message'   => __('complaintClientEmailBody', $templateData, $resellerId),
+                MessagesClient::sendMessage(
+                    [
+                        [ // MessageTypes::EMAIL
+                            'emailFrom' => $emailFrom,
+                            'emailTo' => $client->email,
+                            'subject' => ['complaintClientEmailSubject', $templateData, $mapper->getResellerId()],
+                            'message' => ['complaintClientEmailBody', $templateData, $mapper->getResellerId()],
+                        ],
                     ],
-                ], $resellerId, $client->id, NotificationEvents::CHANGE_RETURN_STATUS, (int)$data['differences']['to']);
+                    $mapper->getResellerId(),
+                    $client->id,
+                    NotificationEvents::CHANGE_RETURN_STATUS,
+                    $mapper->getDifferencesTo()
+                );
                 $result['notificationClientByEmail'] = true;
             }
 
             if (!empty($client->mobile)) {
-                $res = NotificationManager::send($resellerId, $client->id, NotificationEvents::CHANGE_RETURN_STATUS, (int)$data['differences']['to'], $templateData, $error);
+                $res = NotificationManager::send(
+                    $mapper->getResellerId(),
+                    $client->id,
+                    NotificationEvents::CHANGE_RETURN_STATUS,
+                    $mapper->getDifferencesTo(),
+                    $templateData,
+                    $error
+                );
                 if ($res) {
                     $result['notificationClientBySms']['isSent'] = true;
                 }
